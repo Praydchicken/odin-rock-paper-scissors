a02f07c90b58f6f84a2a962c1dc431637d8c9d2c
Mark <111501559+Praydchicken@users.noreply.github.com>
2025-12-21 11:37:08 +0800

    feat: add playRound function add function that takes player and computer choice and determines the winner

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
 
@@ -15,3 +18,25 @@ function getHumanChoice() {
   const humanChoice = prompt('Rock, Paper, or Scissors?');
   return humanChoice.toLowerCase().trim();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice === computerChoice) {
+    return 'draw';
+  }
+
+  const humanWins = (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  );
+
+  if(humanWins) {
+    humanScore++;
+    return 'Player Wins!';
+  } else {
+    computerScore++;
+    return 'Computer Wins!';
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
