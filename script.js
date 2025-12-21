f99d582d2b68a7e183f8309f58a2fafd7fd684f8
Mark <111501559+Praydchicken@users.noreply.github.com>
2025-12-21 11:18:54 +0800

    feat: add getComputerChoice function add function that randomly generates computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3);
+
+  switch(computerChoice) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
