2809b0775e5f61ea97e0830ed0104cb32dda7e39
Mark <111501559+Praydchicken@users.noreply.github.com>
2025-12-21 11:25:31 +0800

    feat: add getHumaChoice function add function that get the user inputs, format it and return its value

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ function getComputerChoice() {
       return 'scissors';
   }
 }
+
+function getHumanChoice() {
+  const humanChoice = prompt('Rock, Paper, or Scissors?');
+  return humanChoice.toLowerCase().trim();
+}
