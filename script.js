f814b4ec36035646fc58fa6d770e9a68c99f5448
Mark <111501559+Praydchicken@users.noreply.github.com>
2025-12-26 17:20:07 +0800

    refactor: change const HumanWins into a helper function

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 function getComputerChoice() {
   const computerChoice = Math.floor(Math.random() * 3);
 
-  switch(computerChoice) {
+  switch (computerChoice) {
     case 0:
-      return 'rock';
+      return "rock";
     case 1:
-      return 'paper';
+      return "paper";
     case 2:
-      return 'scissors';
+      return "scissors";
   }
 }
 
 function getHumanChoice() {
-  const humanChoice = prompt('Rock, Paper, or Scissors?');
+  const humanChoice = prompt("Rock, Paper, or Scissors?");
 
-  if(humanChoice === null) {
+  if (humanChoice === null) {
     return null;
   }
 
   return humanChoice.toLowerCase().trim();
+}
+
+function doesHumanWin(humanChoice, computerChoice) {
+	return (
+		(humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+	);
 }
 
 function playGame() {
@@ -27,17 +35,11 @@ function playGame() {
   let computerScore = 0;
 
   function playRound(humanChoice, computerChoice) {
-    if(humanChoice === computerChoice) {
+    if (humanChoice === computerChoice) {
       return alert(`both chose ${computerChoice}, DRAW!!`);
     }
 
-    const humanWins = (
-      (humanChoice === 'rock' && computerChoice === 'scissors') ||
-      (humanChoice === 'paper' && computerChoice === 'rock') ||
-      (humanChoice === 'scissors' && computerChoice === 'paper')
-    );
-
-    if(humanWins) {
+    if (doesHumanWin(humanChoice, computerChoice)) {
       humanScore++;
       alert(`${humanChoice} beats ${computerChoice}, PLAYER WINS!`);
     } else {
@@ -50,9 +52,11 @@ function playGame() {
     playRound(getHumanChoice(), getComputerChoice());
   }
 
-  if(humanScore === computerScore){
-    alert('Draw');
-  } else if (humanScore > computerScore) {
+  if (humanScore === computerScore) {
+    alert("Draw");
+  }
+
+  if (humanScore > computerScore) {
     alert(`Player Wins the game!
       Player: ${humanScore} Computer: ${computerScore}`);
   } else {
